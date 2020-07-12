3e5ebcf8cb3333bb953dc29d0316756c54bdc0ed
amirAlamian <amiralamian10@gmail.com>
2020-07-12 19:44:05 +0430

    initial settings

--- app.js
+++ app.js
@@ -1,26 +1,47 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose =require("mongoose");
+const api = require('./routes/api');
+const usersRouter = require('./routes/users');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express();
 
-var app = express();
+///////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////view engine setup/////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////
+app.set("view engine", "ejs");
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
 
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////data base setup/////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+// handle mongoose collection.ensureIndex warn
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+
+mongoose.connect('mongodb://localhost:27017/article-bloger-comments', {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+});
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////end points and midlles wares/////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+app.use('/api', api);
+
 
 
 
